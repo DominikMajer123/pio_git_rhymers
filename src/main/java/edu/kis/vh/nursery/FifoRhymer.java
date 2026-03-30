292f7fb3a47b04e657141ebbd57967f049f519f5
DominikMajer123 <255667@edu.p.lodz.pl>
2026-03-30 16:33:53 +0200

    17. Dodanie dokumentacji java do FifoRhymer

--- src/main/java/edu/kis/vh/nursery/FifoRhymer.java
+++ src/main/java/edu/kis/vh/nursery/FifoRhymer.java
@@ -1,9 +1,16 @@
 package edu.kis.vh.nursery;
 
+/**
+ *Klasa reprezentujaca wyliczanke w oparciu o kolejke first in first out
+ */
 public class FifoRhymer extends DefaultCountingOutRhymer {
 
     private final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
-    
+
+    /**
+     * Zwraca i susuwa najstarszy elemtn z kolejki.
+     * * @return wartość pierwszego dodanego elementu lub wartość domyślną, gdy wyliczanka jest pusta
+     */
     public int countOut() {
         while (!callCheck())
             temp.countIn(super.countOut());
